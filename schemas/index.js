c98535a2b262c933ac8c369d66572e9b0f58ccf7
Olawale Orija <waleob@googlemail.com>
2023-03-20 14:58:34 +0000

    first commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,12 @@
-export const schemaTypes = []
+import testimonials from "./testimonials"
+import abouts from "./abouts"
+import brands from "./brands"
+import contacts from "./contact"
+import experiences from "./experiences"
+import works from "./works"
+import workExperience from "./workExperience"
+import skills from "./skills"
+
+
+export const schemaTypes = [testimonials, abouts, brands, 
+    contacts, skills, works, experiences, workExperience]
